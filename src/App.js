5ecdd64661240ffc113acbc0f813e7a043d1a511
Camilo Jimenez <jucajigue+developer@gmail.com>
2022-08-23 12:07:51 -0500

    Add loader component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
 
 import { Home } from "./components/Home";
+import { Loader } from "./components/Loader";
 import { SignIn } from "./components/SignIn";
 import { SignUp } from "./components/SignUp";
 import { useAuth } from "./context/AuthContex";
 
 const App = () => {
-  const { isSignIn } = useAuth();
+  const { isSignIn, isLoading } = useAuth();
 
   const [showSignUp, setShowSignUp] = useState(false)
+
+  if(isLoading)
+    return <Loader />
 
   /* Function reder for each component */
   const renderContent = () => {
